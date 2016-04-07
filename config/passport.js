071178799cbbd7c735676d6356d42e69f605f91c
Luis Felipe Pérez <xilenomg@gmail.com>
2016-04-07 11:29:46 -0300

    Passport - Local login

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,64 @@
+//Loading Strategies
+var LocalStrategy    = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy  = require('passport-twitter').Strategy;
+var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
+
+// load up the user model
+var User       = require('../app/models/user');
+
+// load the auth config
+var configAuth = require('./auth');
+
 module.exports = function(passport) {
+
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+
+    //Local login
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    },
+    function(request, email, password, done) {
+        if (email){
+            email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
+        }
+
+        // asynchronous
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                // if there are any errors, return the error
+                if (err)
+                    return done(err);
+
+                // no user found
+                if (!user)
+                    return console.log('No user found.');
+
+                // invalid password
+                if (!user.validPassword(password))
+                    return console.log('Invalid password');
+
+                // all is well, return user
+                else
+                    return done(null, user);
+            });
+        });
+    }));
 };
